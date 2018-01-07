f628811bb2c4f343d3209ecdca60607d8949f920
xiaolin <462441355@qq.com>
2018-01-07 15:58:13 +0800

    * 导入正则模块  爬取测试

--- spider/spider.py
+++ spider/spider.py
@@ -1,21 +1,32 @@
+# 正则模块
+import re
+# 发送
 from urllib import request
-
 # https 会报错  导入ssl模块，把证书验证关了
 import ssl
+
 ssl._create_default_https_context = ssl._create_unverified_context
 
+
 class Spider():
-
+    # 请求url
     url = 'https://www.panda.tv/cate/lol'
+    root_pattern = '<div class="video-info">([\s\S]*?)</div>'
 
+    # 发送请求
     def __fetch_content(self):
         r = request.urlopen(Spider.url)
         html = r.read()
         html = str(html, encoding='utf-8')
-        print(html)
+        return html
+
+    def __analysis(self, html):
+        root_html = re.findall(Spider.root_pattern, html)
+        print(root_html[0])
 
     def go(self):
-        self.__fetch_content()
+        html = self.__fetch_content()
+        self.__analysis(html)
 
 
 spider = Spider()
