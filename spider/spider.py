8912a120fe71ea11ca2a9267630c97a3d01047f5
xiaolin <462441355@qq.com>
2018-01-07 16:08:00 +0800

    * 匹配主播名字和人气

--- spider/spider.py
+++ spider/spider.py
@@ -12,6 +12,8 @@ class Spider():
     # 请求url
     url = 'https://www.panda.tv/cate/lol'
     root_pattern = '<div class="video-info">([\s\S]*?)</div>'
+    name_pattern = '</i>([\s\S]*?)</span>'
+    number_pattern = '<span class="video-number">([\s\S]*?)</span>'
 
     # 发送请求
     def __fetch_content(self):
@@ -20,13 +22,20 @@ class Spider():
         html = str(html, encoding='utf-8')
         return html
 
+    # 匹配主播名字和人气
     def __analysis(self, html):
         root_html = re.findall(Spider.root_pattern, html)
-        print(root_html[0])
+        anchors = []
+        for html in root_html:
+            name = re.findall(Spider.name_pattern, html)
+            number = re.findall(Spider.number_pattern, html)
+            anchor = {'name': name, 'number': number}
+            anchors.append(anchor)
+        return anchors
 
     def go(self):
         html = self.__fetch_content()
-        self.__analysis(html)
+        anchors = self.__analysis(html)
 
 
 spider = Spider()
