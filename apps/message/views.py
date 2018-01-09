1d2404e22da16707210f04254d85d3173d0b2224
xiaolin <462441355@qq.com>
2018-01-09 23:53:56 +0800

    * 增加个留言板测试

--- apps/message/views.py
+++ apps/message/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# 留言板
+def getform(request):
+    return render(request, 'message_form.html')
--- python/urls.py
+++ python/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
- 
+
 from . import view
- 
+from apps.message.views import getform
+
 urlpatterns = [
     url(r'^$', view.hello),
+    url(r'^form/$', getform)
 ]
