1860df7790a5daefafd2c052c1e3afedbbc48151
xiaolin <462441355@qq.com>
2018-01-13 21:49:27 +0800

    * 配置

--- python/urls.py
+++ python/urls.py
@@ -15,6 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic import TemplateView
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 from apps.message.views import getform
 from extra_apps.xadmin import xadmin
@@ -22,5 +26,8 @@ from extra_apps.xadmin import xadmin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^xadmin/', xadmin.site.urls),
-    url(r'^form/$', getform)
+    url(r'^form/$', getform),
+    url('^$', TemplateView.as_view(template_name="index.html"), name="index"),
+    url('^login/$', TemplateView.as_view(template_name="login.html"), name="login")
 ]
+static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
