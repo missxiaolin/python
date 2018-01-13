67cdb8b403494364fb137aae9a78f56f1acd58d3
xiaolin <462441355@qq.com>
2018-01-13 20:50:33 +0800

    * 过滤搜索测试

--- apps/message/adminx.py
+++ apps/message/adminx.py
@@ -3,16 +3,23 @@ __author__ = 'xiaolin'
 __data__ = '2018/1/13 下午6:48'
 
 from extra_apps.xadmin import xadmin
-from .models import EmailVerifyRecord
-from .models import Banner
+from .models import EmailVerifyRecord, Banner
 
 
 class EmailVerifyRecordAdmin(object):
-    pass
+    # 显示
+    list_display = ['email', 'send_type', 'send_time']
+    # 搜索
+    search_field = ['code', 'email']
+    # 过滤
+    list_filter = ['code', 'email', 'send_time']
+
 
 xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)
 
+
 class BannerAdmin(object):
     pass
+
 
 xadmin.site.register(Banner, BannerAdmin)
