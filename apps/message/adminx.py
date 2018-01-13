a4895c8288a9f096fdf4f6aa2ed4636996522ce3
xiaolin <462441355@qq.com>
2018-01-13 21:05:58 +0800

    * 后台xadmin测试

--- apps/message/adminx.py
+++ apps/message/adminx.py
@@ -5,6 +5,18 @@ __data__ = '2018/1/13 下午6:48'
 from extra_apps.xadmin import xadmin
 from .models import EmailVerifyRecord, Banner
 
+# 主题
+class BaseSetting(object):
+    enable_themes = True
+    use_booswatch = True
+
+class GlobalSetting(object):
+    site_title = '小林'
+    site_bootswatch = True
+
+# xadmin.site.register(xadmin.views.BaseAdminView, BaseSetting)
+# xadmin.site.register(xadmin.views.CommAdminView, GlobalSetting)
+
 
 class EmailVerifyRecordAdmin(object):
     # 显示
