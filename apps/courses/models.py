3746a15abc896352866b475128e0752ed5d7e7c2
xiaolin <462441355@qq.com>
2018-01-13 13:00:49 +0800

    * model 设计

--- apps/courses/models.py
+++ apps/courses/models.py
@@ -24,7 +24,7 @@ class Course(models.Model):
 
 # 章节
 class Lesson(models.Model):
-    course = models.ForeignKey('Course', on_delete=models.CASCADE, verbose_name=u'课程',default=0)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name=u'课程',default=0)
     name = models.CharField(max_length=100, verbose_name=u'章节名')
     add_time = models.DateTimeField(default=datetime.now, verbose_name=u'添加时间')
 
